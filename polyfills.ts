d71b3099e750351a39b1de33534915a8f95771ed
iyyanar2405 <iiyyanarkalivarathan48@gmail.com>
2025-09-06 21:50:00 +0530

    --style sheet error fixed

---
+++ polyfills.ts
@@ -0,0 +1,9 @@
+/***************************************************************************************************
+ * Load `$localize` — Angular's i18n support
+ */
+import '@angular/localize/init';
+
+/***************************************************************************************************
+ * Zone JS is required by Angular
+ */
+import 'zone.js';  // Already listed in package.json dependencies
